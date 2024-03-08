9d00a50e2e0ea6029a582b5db23474afcbe85888
Stephanyy <santosstephany400@gmail.com>
2024-03-08 17:31:49 -0300

    Creating the Preload page

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -6,5 +6,7 @@ const Stack = createStackNavigator();
 export function MainStack() {
     <Stack.Navigator>
         <Stack.Screen name="Preload" component={Preload}/>
+        <Stack.Screen name="SignIn" component={SignIn}/>
+        <Stack.Screen name="SignOut" component={SignOut} />
     </Stack.Navigator>
 };
