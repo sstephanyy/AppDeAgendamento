27b20695baa5ec056f6a5e1a1b3eba3e8171f626
Stephanyy <santosstephany400@gmail.com>
2024-03-08 17:44:27 -0300

    creating the SignUp page

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Text } from 'react-native';
 import { Container } from './style';
 
-export default function SignIn(){
+export default function SignUp(){
     return(
         <Container>
-            <Text>Entrar</Text>
+            <Text>Cadastrar</Text>
         </Container>
     )
 }
