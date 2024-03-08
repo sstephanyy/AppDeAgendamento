1c8ffcecf5e0a8a9d9601fbab1b0b4d5dbce0e30
Stephanyy <santosstephany400@gmail.com>
2024-03-08 17:39:08 -0300

    Creating the SignIn page

---
+++ src/screens/SignIn/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from 'react-native';
+import { Container } from './style';
+
+export default function SignIn(){
+    return(
+        <Container>
+            <Text>Inscrever-se</Text>
+        </Container>
+    )
+}
