2691c7e2736c2a66761868d5a3d17977f8db99cc
Stephanyy <santosstephany400@gmail.com>
2024-03-08 17:46:38 -0300

    import the screens components

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { createStackNavigator } from '@react-navigation/stack';
 
+import PreLoad from "../screens/Preload";
+import SignIn from "../screens/SignIn";
+import SignUp from "../screens/SignUp";
+
 const Stack = createStackNavigator();
 
 export function MainStack() {
     <Stack.Navigator>
-        <Stack.Screen name="Preload" component={Preload}/>
+        <Stack.Screen name="PreLoad" component={PreLoad}/>
         <Stack.Screen name="SignIn" component={SignIn}/>
-        <Stack.Screen name="SignOut" component={SignOut} />
+        <Stack.Screen name="SignUp" component={SignUp} />
     </Stack.Navigator>
 };
