45a82c95999ed9fb4c58c3295c3714170e500259
Stephanyy <santosstephany400@gmail.com>
2024-03-09 11:27:25 -0300

    Checking for token existence on component mount

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Text } from 'react-native';
 import { Container, LoadingIcon } from './style';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
 
 import ComputerLogo from '../../../assets/pointing-to-pc.svg';
 
 export default function PreLoad() {
+
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        const checkToken =  async () => {
+            const token = await AsyncStorage.getItem('token');
+            if(token){
+                //validate the token
+            }
+            else{
+                navigation.navigate('SignIn');
+            }
+        }
+
+    }, []);
+
     return (
         <Container>
             <ComputerLogo width="100%" height="200" fill="white"/>
