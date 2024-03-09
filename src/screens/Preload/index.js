70301681a4fb30c1444b3b17270cf000d1cf24b4
Stephanyy <santosstephany400@gmail.com>
2024-03-09 10:48:36 -0300

    Adding a loading icon to the preload page

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Text } from 'react-native';
-import { Container } from './style';
+import { Container, LoadingIcon } from './style';
 
 import ComputerLogo from '../../../assets/pointing-to-pc.svg';
 
@@ -8,6 +8,7 @@ export default function PreLoad() {
     return (
         <Container>
             <ComputerLogo width="100%" height="200" fill="white"/>
+            <LoadingIcon size="large" color="#FFFFFF" />
         </Container>
     );
 }
