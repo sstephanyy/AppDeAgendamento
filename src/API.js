b863b7e036ad116005424f2917fdf99f1548c1a1
Stephanyy <santosstephany400@gmail.com>
2024-03-14 20:44:42 -0300

    signUp auth

--- src/API.js
+++ src/API.js
@@ -1,3 +1,26 @@
-import { Auth } from "firebase/auth";
+import { FIREBASE_AUTH } from "../FirebaseConfig";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
-expo
+const auth = FIREBASE_AUTH;
+
+export const signUp = async (email, password) => {
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        console.log(userCredential);
+        return { success: true, user: userCredential.user };
+
+    } catch (error) {
+        console.log(error);
+        return { success: false, error };
+    }
+}
+
+export const signIn = async (email, password) => {
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        return { success: true, user: userCredential.user };
+    } catch (error) {
+        console.log(error);
+        return { success: false, error };
+    }
+}
